d5d414c00011017d48fb333cc0379bf70e9d57b1
Richard Loth <r.loth@web.de>
2016-10-06 21:18:06 +0200

    -Added a link to a tutorial how to prepare an account for automated synchronising

--- WpfApplication1/view/EnterAccountName.xaml.cs
+++ WpfApplication1/view/EnterAccountName.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WpfPoeChallengeTracker.model;
 
@@ -100,6 +102,12 @@ namespace WpfPoeChallengeTracker.view
         {
             OkButton.IsEnabled = false;
             statusTextBlock.Visibility = Visibility.Hidden;
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
